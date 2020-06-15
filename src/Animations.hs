8b392f433f10a73648c3fb92f7d6d28ba0172985
Daniel Heurlin <daniel.heurlin@gmail.com>
2020-06-15 02:47:36 +0200

    Extend animation library

---
+++ src/Animations.hs
@@ -0,0 +1,73 @@
+module Animations
+  ( DeltaTime
+  , Frame
+  , Animation(..)
+  , cycleAnimation
+  , TimeStamp
+  , AbsFrame
+  , AbsAnimation
+  , stepAnim
+  , makeFrameEvent
+  , toAbsAnimation
+  , testAnimation
+  )
+where
+
+import Data.Maybe
+
+import           Reactive.Banana
+import           Reactive.Banana.Frameworks
+
+type DeltaTime         = Integer
+type Frame a           = (DeltaTime, a)
+-- | An animation with relative timestaps.
+-- Must be converted to 'AbsAnimation' to be played
+newtype Animation a  =  Animation [Frame a]
+
+unAnimation :: Animation a -> [AbsFrame a]
+unAnimation (Animation xs) = xs
+
+type TimeStamp         = Integer
+type AbsFrame a        = (TimeStamp, a)
+-- | An animation with absolute timestamps which can be played back
+newtype AbsAnimation a = AbsAnimation [AbsFrame a]
+  deriving (Show)
+
+unAbsAnimation :: AbsAnimation a -> [AbsFrame a]
+unAbsAnimation (AbsAnimation xs) = xs
+
+cycleAnimation :: Maybe DeltaTime -> Animation a -> Animation a
+cycleAnimation pause (Animation xs) = Animation $ zip cTimes cFrames
+ where
+  (times@(t : ts), frames) = unzip xs
+  cFrames                  = cycle frames
+  cTimes                   = times ++ cycle ((t + fromMaybe 1 pause) : ts)
+
+-- | Advances the animation by one step if the correct point in time has been
+-- reached, and returns (currentFrame, rest). If the correct point in time
+-- has not been reached, returns (Nothing, rest)
+stepAnim :: TimeStamp -> (Maybe a, AbsAnimation a) -> (Maybe a, AbsAnimation a)
+stepAnim now (_, AbsAnimation []) = (Nothing, AbsAnimation [])
+stepAnim now (_, (AbsAnimation anim@((time, a) : xs)))
+  | now == time = (Just a, AbsAnimation xs)
+  | otherwise   = (Nothing, AbsAnimation anim)
+
+makeFrameEvent :: Event TimeStamp -> AbsAnimation a -> MomentIO (Event a)
+makeFrameEvent eCounter anim = do
+  eAnimation <- accumE (Nothing, anim) $ stepAnim <$> eCounter
+  pure $ fromJust . fst <$> filterE (isJust . fst) eAnimation
+
+toAbsAnimation :: Animation a -> AbsAnimation a
+toAbsAnimation (Animation xs) = AbsAnimation $ zip absoluteTimes frames
+ where
+  (times, frames) = unzip xs
+  absoluteTimes   = tail $ scanl (+) 0 times
+
+
+testAnimation :: AbsAnimation String
+testAnimation = AbsAnimation
+  [ (0, "Det var en gång en bajsman")
+  , (1, "Han hette bajsmannen")
+  , (3, "En gång skulle han bajsa...")
+  , (6, "Han gjorde det i NC")
+  ]
--- src/FRP.hs
+++ src/FRP.hs
@@ -6,6 +6,7 @@ import           Util
 import           Midi
 import           Notes
 import           DataTypes
+import           Animations
 
 import           Control.Monad
 import           Data.Maybe
@@ -29,6 +30,14 @@ import           Reactive.Banana.Frameworks
 
 import qualified Sound.PortMidi                as PM
 
+ta :: Animation String
+ta = cycleAnimation Nothing $ Animation
+  [ (0, "Sagan om bajsmannen, del 2")
+  , (1, "Den här gången lite bajsenödigare")
+  , (1, "En kort paus nu då...")
+  , (4, "..Och så kör vi igen!")
+  ]
+
 
 makeNetworkDescription
   :: AddHandler () -> AddHandler [MidiMessage] -> MomentIO ()
@@ -39,15 +48,14 @@ makeNetworkDescription addTickEvent addMidiEvent = do
   eTick      <- fromAddHandler addTickEvent
   -- By having an Integer counter, we should never get an overflow
   eCtr       <- accumE (-1 :: Integer) $ eTick $> (+ 1)
-  eFrame     <- makeFrameEvent eCtr testAnimation
+  -- eFrame     <- makeFrameEvent eCtr testAnimation
+  eFrame     <- makeFrameEvent eCtr (toAbsAnimation ta)
 
   reactimate'
     $ fmap (putStrLn <$> ("Held notes: " <>) . show . mapMaybe midiToPianoNote)
     <$> eHeld
 
-
-  -- reactimate' $ fmap putStrLn <$> eFrame
-  reactimate $ print <$> eCtr
+  -- reactimate $ print <$> eCtr
   reactimate $ putStrLn <$> eFrame
 
 
@@ -83,30 +91,3 @@ makeTick = do
 
   pure (addTickEvent, tick, tid)
 
-type TimeStamp   = Integer
-type Frame a     = (TimeStamp, a)
-type Animation a = [Frame a]
-
-
--- | Advances the animation by one step if the correct point in time has been
--- reached, and returns (currentFrame, rest). If the correct point in time
--- has not been reached, returns (Nothing, rest)
-stepAnim :: TimeStamp -> (Maybe a, Animation a) -> (Maybe a, Animation a)
-stepAnim now (_, [])                  = (Nothing, [])
-stepAnim now (_, anim@((time, a):xs))
-  | now == time = (Just a, xs)
-  | otherwise   = (Nothing, anim)
-
-makeFrameEvent :: Event TimeStamp -> Animation a -> MomentIO (Event a)
-makeFrameEvent eCounter anim = do
-  eAnimation <- accumE (Nothing, anim) $ stepAnim <$> eCounter
-  pure $ fromJust . fst <$> filterE (isJust . fst) eAnimation
-
-
-testAnimation :: Animation String
-testAnimation =
-  [ (0, "Det var en gång en bajsman")
-  , (1, "Han hette bajsmannen")
-  , (3, "En gång skulle han bajsa...")
-  , (6, "Han gjorde det i NC")
-  ]
